e108a7771b423f380a982bfa71c1a79b354a4918
don <don.harbin@linaro.org>
2023-09-27 11:33:00 -0700

    Update search_all_year.py

--- search_all_year.py
+++ search_all_year.py
@@ -1,6 +1,9 @@
 import subprocess
 import search_string 
 
+###
+# Each of the following are run once at a time with results moved to excel file for charting
+###
 #download_file="dragonboard410c/qualcomm/android/\*/dragonboard410c_sdcard_install_android-118.zip"
 #download_file="dragonboard410c/linaro/debian/\*/Image"
 #download_file="dragonboard410c/qualcomm/android/"
@@ -10,8 +13,11 @@ import search_string
 #download_file="dragonboard820c/qualcomm/firmware/linux-board-support-package\*"
 #download_file="/dragonboard820c/qualcomm/firmware"
 #download_file="dragonboard845c/linaro/debian/\*/boot-linaro\*.img.gz"
+#download_file="dragonboard845c/linaro/openembedded/\*/source-manifest.xml"
 
-download_file="dragonboard845c/linaro/openembedded/\*/source-manifest.xml"
+## This one captures both /rpb and /rpb-wayland subdirectories
+download_file="rb5/linaro/openembedded/\*/boot-\*rb5\*.img"
+
 
 subprocess.call("python3 search_string.py /Users/donharbin/Downloads/dragonboard_or_rb_downloads.csv 2020- {}".format(download_file),shell=True)
 subprocess.call("python3 search_string.py /Users/donharbin/Downloads/dragonboard_or_rb_downloads.csv 2021- {}".format(download_file),shell=True)
